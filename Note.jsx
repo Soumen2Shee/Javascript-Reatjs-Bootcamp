4833e4bfa9aaa81845edd02093ab8b0279003cfe
Soumen2Shee <85429209+Soumen2Shee@users.noreply.github.com>
2021-06-06 13:23:48 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Background</h1>
+      <p>
+        Hi I am Soumen Shee. I hail from Contai, Purba Medinipur, West Bengal. I
+        am currently a second year student of Computer Science and Engineering.
+        I like to spend my free time exploring hobbies such as planting and
+        caring for trees, and saving water. I have three members in my family. I
+        dream of working as a software engineer at IBM.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
